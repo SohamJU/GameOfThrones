aaf79833b6e4c46f6846b590935abee652483bec
SohamJU <47265986+SohamJU@users.noreply.github.com>
2019-04-15 17:09:09 +0530

    Update GOT Centrality Measures.R

--- GOT Centrality Measures.R
+++ GOT Centrality Measures.R
@@ -1,11 +1,14 @@
 library(igraph)
-file=read.csv("stormofswords.csv")
-file
+file=read.csv("OverSeries.csv")
+View(file)
 graph=graph_from_data_frame(file,directed=FALSE)
 plot(graph)
 DG=degree(graph)
 DG
 head(sort(DG,decreasing=TRUE),10)
+EG=eigen_centrality(graph)
+EG=EG$vector#only numeric part of list is needed
+head(sort(EG,decreasing=TRUE),10)
 CC=closeness(graph)
 CC
 head(sort(CC,decreasing=TRUE),10)
@@ -16,17 +19,16 @@ avgclusco=transitivity(graph)
 avgclusco
 clus_co=transitivity(graph,type='local')
 clus_co
-clus_co[is.nan(clus_co)]='Inf'
-clus_co
 ecc=eccentricity(graph)
 ecc
 PR=page_rank(graph)
-PR
-PR=as.numeric(as.character(unlist(PR))) ## removing string from list to make it numeric
-PR=PR[1:108] #last value is bogus
-PR
-combo=cbind(DG,CC,BC,ecc,clus_co,PR)
-round(combo, digits=3)
-colnames(combo)=c('Degree','Closeness Centrality', 'Betweenness Centrality','Eccentricity','Clustering Co-efficient','Page Rank')
-combo
+PR=PR$vector #only numeric part of list is needed
+combo=cbind(DG,CC,BC,ecc,clus_co,PR,EG)
+colnames(combo)=c('Degree','Closeness Centrality', 'Betweenness Centrality','Eccentricity','Clustering Co-efficient','Page Rank','Eigen Value Centrality')
+combo=as.data.frame(combo) 
+write.csv(combo,'prediciton.csv')
+#Small World Phenomenon
+mean(combo$Eccentricity)
+# Power law fit for Degree Distribution
+fit_power_law(DG)
 
